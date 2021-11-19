cbf061508571204faf689feab4926c4f91c9b65c
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-11-19 10:22:40 +0800

    add decoration

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -32,87 +32,103 @@ class _HomeScreenState extends State<SignupScreen> {
         backgroundColor: color.AppColor.bluishClr,
       ),
       body: SingleChildScrollView(
-        child: Stack(
-          children: <Widget>[
-            Center(
-              child: Column(
-                children: const [
-                  SizedBox(height: 50),
-                  Text('Create an account, its free',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ],
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 40, right: 40),
-                  child: Form(
-                    key: formKey,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 120),
-                        TextFormField(
-                          controller: nameController,
-                          decoration: const InputDecoration(
-                            // border: OutlineInputBorder(),
-                            labelText: 'Enter your name',
-                            labelStyle: TextStyle(color: Colors.grey),
-                          ),
-                        ),
-                        TextFormField(
-                          controller: emailController,
-                          decoration: const InputDecoration(
-                              labelText: 'Enter your email',
-                              labelStyle: TextStyle(color: Colors.grey)),
-                        ),
-                        TextFormField(
-                          controller: passwordController,
-                          decoration: const InputDecoration(
-                              labelText: 'Enter your phone number',
-                              labelStyle: TextStyle(color: Colors.grey)),
-                        ),
-                        TextFormField(
-                          obscureText: hidePassword,
-                          controller: passwordController,
-                          decoration: const InputDecoration(
-                              labelText: 'Enter your password',
-                              labelStyle: TextStyle(color: Colors.grey)),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 40, right: 40),
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Center(
                 child: Column(
-                  children: [
-                    const SizedBox(height: 340),
-                    Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          color: color.AppColor.bluishClr,
-                          borderRadius: BorderRadius.circular(5.0)),
-                      child: TextButton(
-                        onPressed: () async {},
-                        child: const Text(
-                          'Sign Up',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
+                  children: const [
+                    SizedBox(height: 50),
+                    Text('Create an account, its free',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        )),
                   ],
                 ),
               ),
-            )
-          ],
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40, right: 40),
+                    child: Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: nameController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter your name',
+                              labelStyle: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: emailController,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Enter your email',
+                                labelStyle: TextStyle(color: Colors.grey)),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: passwordController,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Enter your phone number',
+                                labelStyle: TextStyle(color: Colors.grey)),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            obscureText: hidePassword,
+                            controller: passwordController,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Enter your password',
+                                labelStyle: TextStyle(color: Colors.grey)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 40),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: color.AppColor.bluishClr,
+                            borderRadius: BorderRadius.circular(5.0)),
+                        child: TextButton(
+                          onPressed: () async {},
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
